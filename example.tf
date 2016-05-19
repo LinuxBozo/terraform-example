e3cb82cae0be58c1c87c88bd3907ee274e1fd149
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-19 11:10:56 -0400

    Simple terraform example with concourse pipeline

---
+++ example.tf
@@ -0,0 +1,29 @@
+/*
+ * Assume we'll provide environment vars:
+ * AWS_ACCESS_KEY_ID
+ * AWS_SECRET_ACCESS_KEY
+ * AWS_DEFAULT_REGION
+ */
+provider "aws" {
+}
+
+# Create a VPC
+resource "aws_vpc" "example_vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags {
+        Name = "example infrastructure"
+        Environment = "Example"
+    }
+}
+
+# Create an S3 bucket to store things
+resource "aws_s3_bucket" "example_bucket" {
+    bucket = "example_bucket"
+    acl = "private"
+
+    tags {
+        Name = "example infrastructure"
+        Environment = "Example"
+    }
+}
